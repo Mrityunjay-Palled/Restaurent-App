473e200d1cc65feb5eaeda740defa3ab5e495fd2
Mrityunjay-Palled <113463073+Mrityunjay-Palled@users.noreply.github.com>
2023-01-31 21:56:57 +0530

    added fully responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,87 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LobsterIcon from "../assets/Lobster-Logo.png";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 const Navbar = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  const [closeMenu, setCloseMenu] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
+  const handleToggle = () => {
+    setToggleDrawer(!toggleDrawer);
+    setCloseMenu(!closeMenu);
+  };
 
   return (
     <nav className="my-4 h-[4rem] shadow-[0px_0px_5px_0px_rgba(0,0,0,0.56)] sm:flex justify-between">
-      <div className="h-full flex items-center">
+      <div className="h-full flex items-center justify-between">
         <img
           className="rotate-90 h-full"
           src={LobsterIcon}
           alt="lobster_logo"
         />
-        <p className="font-merienda text-3xl ml-[3rem] text-red-600 font-extrabold invisible sm:visible">
+        <p className="font-merienda text-3xl ml-[3rem] text-red-600 font-extrabold">
           RED LOBSTER
         </p>
         <span
           className="text-3xl mx-[15px] text-red-600  visible sm:hidden"
-          onClick={() => setIsMobile((prev) => !prev)}
+          onClick={handleToggle}
         >
-          <GiHamburgerMenu />
+          {closeMenu ? <AiOutlineClose /> : <GiHamburgerMenu />}
         </span>
       </div>
       <div className="sm:flex items-center sm:w-[40%] w-full">
-        <div className="sm:flex w-full justify-around text-red-600 font-merienda font-bold flex-wrap mt-[0px] h-[50vh] sm:h-[0px] sm:mt-[0px] leading-[80px] sm:leading-[0px] bg-white z-0 relative">
-          <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
-            MENU
-          </p>
-          <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
-            SPECIALS
-          </p>
-          <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
-            AWARDS
-          </p>
-          <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
-            OUR STORY
-          </p>
-        </div>
+        {!activeMenu ? (
+          <div
+            className={`sm:flex w-full justify-around text-red-600 font-merienda font-bold flex-wrap mt-[0px] h-[50vh] sm:h-[0px] sm:mt-[0px] leading-[80px] sm:leading-[0px] bg-white z-0 relative sm:opacity-100 ${
+              !toggleDrawer ? "translate-x-[800px]" : "translate-x-0"
+            }`}
+          >
+            <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
+              MENU 
+            </p>
+            <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
+              SPECIALS
+            </p>
+            <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
+              AWARDS
+            </p>
+            <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
+              OUR STORY
+            </p>
+          </div>
+        ) : (
+          <div
+            className={`sm:flex w-full justify-around text-red-600 font-merienda font-bold flex-wrap mt-[0px] h-[50vh] sm:h-[0px] sm:mt-[0px] leading-[80px] sm:leading-[0px] bg-white z-0 relative sm:opacity-100`}
+          >
+            <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
+              MENU
+            </p>
+            <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
+              SPECIALS
+            </p>
+            <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
+              AWARDS
+            </p>
+            <p className="mx-[40px] sm:mx-[0px] hover:animate-bounce cursor-pointer">
+              OUR STORY
+            </p>
+          </div>
+        )}
       </div>
     </nav>
   );
