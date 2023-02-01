339a693c4c74261a6d347334aa9904a9f7549dc8
Mrityunjay-Palled <113463073+Mrityunjay-Palled@users.noreply.github.com>
2023-02-02 00:04:58 +0530

    comments removed

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -5,14 +5,8 @@ import Marquee from "react-fast-marquee";
 const VideoSection = () => {
   return (
     <>
-      {/* <p align="center" className="py-6 text-xs sm:text-2xl font-bold font-merienda text-red-600">
-        Traceable, sustainable and responsible applies to so much more than how
-        we catch our seafood.
-      </p> */}
       <Marquee speed="40">
-        <p
-          className="py-4 sm:py-6 text-xs sm:text-2xl font-bold font-merienda text-red-600"
-        >
+        <p className="py-4 sm:py-6 text-xs sm:text-2xl font-bold font-merienda text-red-600">
           Traceable, sustainable and responsible applies to so much more than
           how we catch our seafood.
         </p>
@@ -24,7 +18,10 @@ const VideoSection = () => {
             height="15rem"
             url="https://youtu.be/w7_wnwjG_yQ"
           />
-          <p align="center" className="text-xl pt-2 font-merienda text-white bg-red-400">
+          <p
+            align="center"
+            className="text-xl pt-2 font-merienda text-white bg-red-400"
+          >
             GEAR MARKING
           </p>
         </div>
@@ -34,7 +31,10 @@ const VideoSection = () => {
             height="15rem"
             url="https://youtu.be/6gtu0IJyoLQ"
           />
-          <p align="center" className="text-xl pt-2 font-merienda text-white bg-red-400">
+          <p
+            align="center"
+            className="text-xl pt-2 font-merienda text-white bg-red-400"
+          >
             LOBSTERMANIA-2
           </p>
         </div>
@@ -44,7 +44,10 @@ const VideoSection = () => {
             height="15rem"
             url="https://youtu.be/fLzTkPSQnKI"
           />
-          <p align="center" className="text-xl pt-2 font-merienda text-white bg-red-400">
+          <p
+            align="center"
+            className="text-xl pt-2 font-merienda text-white bg-red-400"
+          >
             MASTER CHEF
           </p>
         </div>
