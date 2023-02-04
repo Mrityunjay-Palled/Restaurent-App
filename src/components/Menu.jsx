7d87d74dbfffaa6a5609742910f5f0214e9e03d5
Mrityunjay-Palled <113463073+Mrityunjay-Palled@users.noreply.github.com>
2023-02-04 22:34:25 +0530

    added Lobserfest sample data and updated menu accordingly

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Background_Image from "../assets/Background_Image.jpg";
 import Menu_1 from "../assets/Menu_1.jpg"
+import _placeholder from "../assets/_placeholder.jpg"
+import { data } from "./data";
 
 const Menu = () => {
+  const cardData=data.LobsterFest
   return (
     <div style={{ backgroundImage: `url(${Background_Image})` }}>
-      <div className="bg-white w-[20rem] h-[20rem] sm:h-[25rem] shadow-[0px_0px_10px_0px_rgba(0,0,0,0.56)] m-5">
-        <img src={Menu_1} alt="" className="h-[40%] w-[100%]"/>
-        <div className="p-3">
-        <p align="center" className="font-bold text-xl">Lobster Lover's Dream®</p>
-        <p align="center" className="text-gray-500 font-[15px] leading-[15px]">Roasted rock and Maine lobster tails paired with lobster-and-shrimp linguini in a creamy lobster sauce. Served with choice of two sides</p>
-        </div>
-        <div align="center" className="mt-[1rem] sm:mt-[4rem] px-[1rem]">
-          <button className="bg-[#DF2140] py-[10px] px-[40px] rounded-[10px] text-[18px] font-[600] cursor-pointer text-[#fff] w-[100%]">Order Now</button>
-        </div>
+      <div className="grid grid-cols-1 justify-items-center sm:grid-cols-4">
+      {cardData.map((item)=>(
+           <div className="bg-white w-[20rem] h-[20rem] sm:h-[25rem] shadow-[0px_0px_10px_0px_rgba(0,0,0,0.56)] m-5">
+           <img src={item.img===""?_placeholder:item.img} alt="" className="h-[40%] w-[100%]"/>
+           <div className="p-3 h-[35%] sm:h-[30%]">
+           <p align="center" className="font-bold text-xl whitespace-nowrap overflow-hidden overflow-ellipsis">{item.title}</p>
+           <p align="center" className="text-gray-500 font-[15px] leading-[15px]">{item.description}</p>
+           </div>
+           <div align="center" className="mt-[1rem] sm:mt-[4rem] px-[1rem] flex flex-col justify-end">
+             <button className="bg-[#DF2140] py-[10px] px-[40px] rounded-[10px] text-[18px] font-[600] cursor-pointer text-[#fff] w-[100%]">Order Now</button>
+           </div>
+         </div>
+      ))}
       </div>
     </div>
   );
---
+++ src/components/data.jsx
@@ -0,0 +1,44 @@
+export const data={
+  LobsterFest:[
+    {
+        img:"https://img-ecomm-rl-prod.azureedge.net/ecomm-image/640x360/0002925M_9C97DF738C434CDFE053046613AC0924.jpg",
+        title:"Lobster Lover's Dream®",
+        description:"Roasted rock and Maine lobster tails paired with lobster-and-shrimp linguini in a creamy lobster sauce. Served with choice of two sides.",
+    },
+    {
+        img:"",
+        title:"Lobster Lover's Dream® with Shrimp Alfredo",
+        description:"Roasted rock and Maine lobster tails paired with Shrimp Linguini Alfredo. Served with choice of two sides."
+    },
+    {
+        img:"",
+        title:"Lobster Lover's Dream® with Cajun Chicken Alfredo",
+        description:"Roasted rock and Maine lobster tails paired with Cajun Chicken Linguini Alfredo. Served with choice of two sides."
+    },
+    {
+        img:"https://img-ecomm-rl-prod.azureedge.net/ecomm-image/640x360/0030045M_F1ED56C31E82794FE053077A13AC1B1E.jpg",
+        title:"New! Lobster & Shrimp Tacos",
+        description:"Crispy Maine & Norway lobster and shrimp, with jalapeño slaw, pico de gallo, lime crema and cilantro. Served on three grilled tortillas."
+    },
+    {
+        img:"https://img-ecomm-rl-prod.azureedge.net/ecomm-image/640x360/0030047M_F1ED6BC25ECB8ACFE053077A13ACD932.jpg",
+        title:"New! Lobster & Shrimp-Topped Sirloin**",
+        description:"A tender Maine lobster tail paired with a 7 oz. sirloin topped with creamy parmesan-bacon shrimp. Served with choice of two sides."
+    },
+    {
+        img:"https://img-ecomm-rl-prod.azureedge.net/ecomm-image/640x360/0030057M_F1ED80B47C78AB6AE053077A13ACAB91.jpg",
+        title:"New! Maine Lobster Tail Duo",
+        description:"A pair of tender Maine lobster tails, one roasted and one grilled. Served with choice of two sides.",
+    },
+    {
+        img:"https://img-ecomm-rl-prod.azureedge.net/ecomm-image/640x360/0002948M_5A4252D9CFEE737FE053046613AC9B28.jpg",
+        title:"Bar Harbor Lobster Bake",
+        description:"Petite Maine lobster tails with shrimp, bay scallops, mussels and fresh tomatoes. Served over linguini in a garlic and white wine broth.",
+    },
+    {
+        img:"https://img-ecomm-rl-prod.azureedge.net/ecomm-image/640x360/0002395M_5A4252D9CFC9737FE053046613AC9B28.jpg",
+        title:"Live Maine Lobster (1.25 lb.)",
+        description:"Wild-caught, fresh from the Atlantic. Prepared to order in the classic style or roasted with crab-and-seafood stuffing. Served with choice of two sides. Not available for raw purchase.",
+    }
+  ]
+}
